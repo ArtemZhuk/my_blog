61406acb6d28cfc7e9813b148780a4e98d400ead
Artem <artemzhuk.kh@gmail.com>
2016-03-29 13:36:57 +0300

    edit migrations

--- db/migrate/20160302090935_create_articles.rb
+++ db/migrate/20160302090935_create_articles.rb
@@ -1,20 +1,13 @@
 class CreateArticles < ActiveRecord::Migration
-  def up
-      drop_table :articles
-      
-      create_table :articles do |t|
-      t.string   "title"
-    	t.text     "text"
-    	t.integer  "user_id"
-    	t.datetime "created_at",  null: false
-    	t.datetime "updated_at",  null: false
+  def change
+    create_table :articles do |t|
+    t.string   "title"
+  	t.text     "text"
+  	t.integer  "user_id"
+  	t.datetime "created_at",  null: false
+  	t.datetime "updated_at",  null: false
 
-      t.timestamps null: false
+    t.timestamps null: false  
     end
   end
-
-  def change
-    add_column :article, :expired, :boolean, default: false
-  end
-
 end
--- db/migrate/20160304085227_create_users.rb
+++ db/migrate/20160304085227_create_users.rb
@@ -1,8 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
 
-  def up
-    drop_table :users
-
+  def change
     create_table :users do |t|
       t.string   "email"
     	t.string   "first_name"
